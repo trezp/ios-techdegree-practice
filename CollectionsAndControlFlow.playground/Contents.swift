6a247927cd4109c14ca7a90b7f850c490748eb95
Treasure Porth <treasure.rose@gmail.com>
2016-06-17 23:39:13 -0700

    dictionaries

--- CollectionsAndControlFlow.playground/Contents.swift
+++ CollectionsAndControlFlow.playground/Contents.swift
@@ -27,3 +27,39 @@ todo.count
 var arrayOfInts = [1,2,3,4,5,6]
 arrayOfInts.append(7)
 arrayOfInts += [8]
+
+//let lastItem = todo[7]
+
+//Dictionaries 
+
+/*
+        Airport Code                    Airport Name
+ 
+        LGA                             La Guardia 
+ 
+ */
+
+var airportCodes = ["LGA": "La Guardia",
+                    "LHR": "Heathrow",
+                    "CDG": "Charles De Gaulle",
+                    "HKG": "Hong Kong International",
+                    "DXB": "Dubai International"
+]
+
+airportCodes["LGA"]
+
+//Inserting Key Value Pairs
+airportCodes["SYD"] = "Sydney Airport"
+
+airportCodes.updateValue("Dublin Airport", forKey: "DUB")
+
+//Removing KEy Value Pairs 
+airportCodes["DXB"] = nil
+airportCodes.removeValueForKey("DUB")
+
+//Dealing with non-existant data 
+
+let airportName = airportCodes["LGA"]
+
+let orlandaAirport = airportCodes["MCO"]
+
